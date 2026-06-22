447a1126284d25324f20fdd2bfb442c1f6bde631
kittiwake <kittiwake@mail.ru>
2026-06-22 11:50:59 +0300

    мерджим на гитхабе

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Header from './components/header/header'
-import Footer from './components/footer/footer'
-import Content from './components/content/content'
-
 function App() {
   return (
-    <div className="app container py-4">
-        <Header />
-        <Content />
-        <Footer />
-</div>
-
+  <h1>Hello</h1>
   )
 }
 
